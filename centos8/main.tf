c0ba9fca15b5e0ddbd9f7739a1ed640f1540d0fd
hvad <david.hannequin@gmail.com>
2020-04-11 19:40:50 +0200

    Centos 8 minimal install.

--- centos8/main.tf
+++ centos8/main.tf
@@ -2,16 +2,16 @@ provider "libvirt" {
   uri = "qemu:///system"
 }
 
-resource "libvirt_volume" "centos-8-qcow2" {
-  name   = "centos-8.qcow2"
+resource "libvirt_volume" "centos-minimal-qcow2" {
+  name   = "centos-minimal.qcow2"
   pool   = "images"
-  source = "/var/lib/libvirt/images/centos8.qcow2"
+  source = "/var/lib/libvirt/images/centos8-minimal.qcow2"
   format = "qcow2"
 }
 
-resource "libvirt_domain" "centos" {
-  name   = "centos8"
-  memory = "4096"
+resource "libvirt_domain" "centos-minimal" {
+  name   = "centos-minimal"
+  memory = "2048"
   vcpu   = 2
 
   network_interface {
@@ -19,7 +19,7 @@ resource "libvirt_domain" "centos" {
   }
 
   disk {
-    volume_id = libvirt_volume.centos-8-qcow2.id
+    volume_id = libvirt_volume.centos-minimal-qcow2.id
   }
 
   console {
